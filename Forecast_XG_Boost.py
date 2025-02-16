02caf1dc5c054ef2bbd4dc81a8356554fea1b357
ecoakshat <168974460+ecoakshat@users.noreply.github.com>
2025-02-16 21:04:36 +0530

    Update Forecast_XG_Boost.py

--- Forecast_XG_Boost.py
+++ Forecast_XG_Boost.py
@@ -51,3 +51,67 @@ from skforecast.model_selection import bayesian_search_forecaster
 from skforecast.model_selection import backtesting_forecaster
 from skforecast.feature_selection import select_features
 from skforecast.preprocessing import RollingFeatures
+
+# Warnings configuration
+# ==============================================================================
+import warnings
+warnings.filterwarnings('once')
+
+color = '\033[1m\033[38;5;208m' 
+print(f"{color}Version skforecast: {skforecast.__version__}")
+print(f"{color}Version scikit-learn: {sklearn.__version__}")
+print(f"{color}Version lightgbm: {lightgbm.__version__}")
+print(f"{color}Version xgboost: {xgboost.__version__}")
+print(f"{color}Version catboost: {catboost.__version__}")
+print(f"{color}Version pandas: {pd.__version__}")
+print(f"{color}Version numpy: {np.__version__}")
+
+# Downloading data
+# ==============================================================================
+data = fetch_dataset('bike_sharing', raw=True)
+data.head()
+
+# Preprocessing data (setting index and frequency)
+# ==============================================================================
+data = data[['date_time', 'users', 'holiday', 'weather', 'temp', 'atemp', 'hum', 'windspeed']]
+data['date_time'] = pd.to_datetime(data['date_time'], format='%Y-%m-%d %H:%M:%S')
+data = data.set_index('date_time')
+
+if pd.__version__ < '2.2':
+    data = data.asfreq('H')
+else:
+    data = data.asfreq('h')
+
+data = data.sort_index()
+data.head()
+
+end_train = '2012-04-30 23:59:00'
+end_validation = '2012-08-31 23:59:00'
+
+data_train = data.loc[: end_train, :]
+data_val   = data.loc[end_train:end_validation, :]
+data_test  = data.loc[end_validation:, :]
+
+print(f"Dates train      : {data_train.index.min()} --- {data_train.index.max()}  (n={len(data_train)})")
+print(f"Dates validacion : {data_val.index.min()} --- {data_val.index.max()}  (n={len(data_val)})")
+print(f"Dates test       : {data_test.index.min()} --- {data_test.index.max()}  (n={len(data_test)})")
+
+# Interactive plot of time series
+# ==============================================================================
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data_train.index, y=data_train['users'], mode='lines', name='Train'))
+fig.add_trace(go.Scatter(x=data_val.index, y=data_val['users'], mode='lines', name='Validation'))
+fig.add_trace(go.Scatter(x=data_test.index, y=data_test['users'], mode='lines', name='Test'))
+fig.update_layout(
+    title  = 'Number of users',
+    xaxis_title="Time",
+    yaxis_title="Users",
+    legend_title="Partition:",
+    width=750,
+    height=350,
+    margin=dict(l=20, r=20, t=35, b=20),
+    legend=dict(orientation="h", yanchor="top", y=1, xanchor="left", x=0.001)
+)
+#fig.update_xaxes(rangeslider_visible=True)
+fig.show()
